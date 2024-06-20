544d3b4bfb0121d6a0031594fd026e1de1431e01
sara <sarah.abiidar@gmail.com>
2024-06-20 12:20:34 +0200

    delet schema in main

--- db/models/Place.js
+++ db/models/Place.js
@@ -1,13 +1,1 @@
 import mongoose from "mongoose";
-
-const { Schema } = mongoose;
-const placeSchema = new Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  image: { type: String, required: true },
-  mapURL: { type: String, required: true },
-  escription: { type: String, required: true },
-});
-// this is a model... this will be for doing CRUD operations
-const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
-export default Place;
